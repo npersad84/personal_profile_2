0fe8269fea8102da49c35ec7bd3c3852dcf1b637
npersad84@gmail.com <npersad84@gmail.com>
2018-10-14 16:38:36 -0400

    Start substatntive code - Using react as front end framework

---
+++ app/services/home_page_data_service.rb
@@ -0,0 +1,12 @@
+class HomePageDataService
+
+  def initialize()
+
+  end
+
+  def body_background
+    'geisha_1.png'
+  end
+
+
+end
---
+++ app/views/home/_body.json.jbuilder
@@ -0,0 +1,3 @@
+json.body_background do
+  json.image_path asset_path(@home_page.body_background)
+end
