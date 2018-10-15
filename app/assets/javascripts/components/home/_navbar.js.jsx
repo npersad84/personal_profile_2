c320fbe15a6d1057e79d8a6e90a4c9de7af8c0cc
Navin Persad <npersad84@gmail.com>
2018-10-14 21:22:05 -0400

    Start navbar

--- app/assets/javascripts/components/home/_navbar.js.jsx
+++ app/assets/javascripts/components/home/_navbar.js.jsx
@@ -1,10 +1,11 @@
 var Navbar = React.createClass({
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div>
-          <p>Hello World</p>
-        </div>
+      <nav className="navbar sticky-top navbar-dark">
+
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
       </nav>
     );
   }
